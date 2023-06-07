929c4aaa195f95f7693703a510aacf16b0b0f968
Vladimir Bachinsky <v.bachinsky@kralys.ch>
2023-06-07 15:37:25 +0300

    MySQL dump updated

--- docker/database/doctor-appointment-system.sql
+++ docker/database/doctor-appointment-system.sql
@@ -1,3 +1,220 @@
+-- --------------------------------------------------------
+-- Сервер:                       127.0.0.1
+-- Версія сервера:               8.0.33 - MySQL Community Server - GPL
+-- ОС сервера:                   Linux
+-- HeidiSQL Версія:              11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
 -- Dumping database structure for doctor-appointment-system
 CREATE DATABASE IF NOT EXISTS `doctor-appointment-system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
--- USE `doctor-appointment-system`;
+USE `doctor-appointment-system`;
+
+-- Dumping structure for таблиця doctor-appointment-system.failed_jobs
+CREATE TABLE IF NOT EXISTS `failed_jobs` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.failed_jobs: ~0 rows (приблизно)
+DELETE FROM `failed_jobs`;
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.migrations: ~0 rows (приблизно)
+DELETE FROM `migrations`;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+	(1, '2014_10_12_000000_create_users_table', 1),
+	(2, '2014_10_12_100000_create_password_resets_table', 1),
+	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+	(5, '2023_06_07_113057_create_roles_table', 1),
+	(6, '2023_06_07_113107_create_permissions_table', 1),
+	(7, '2023_06_07_113225_create_users_permissions_table', 1),
+	(8, '2023_06_07_113301_create_users_roles_table', 1),
+	(9, '2023_06_07_113335_create_roles_permissions_table', 1);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.password_resets
+CREATE TABLE IF NOT EXISTS `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.password_resets: ~0 rows (приблизно)
+DELETE FROM `password_resets`;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.permissions
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.permissions: ~9 rows (приблизно)
+DELETE FROM `permissions`;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
+	(1, 'Manage users', 'manage-users', '2023-06-07 12:10:00', '2023-06-07 12:10:00'),
+	(2, 'Create Appointments', 'create-appointments', '2023-06-07 12:10:00', '2023-06-07 12:10:00'),
+	(3, 'Manage Appointments', 'manage-appointments', '2023-06-07 12:10:00', '2023-06-07 12:10:00'),
+	(4, 'Manage users', 'manage-users', '2023-06-07 12:10:41', '2023-06-07 12:10:41'),
+	(5, 'Create Appointments', 'create-appointments', '2023-06-07 12:10:41', '2023-06-07 12:10:41'),
+	(6, 'Manage Appointments', 'manage-appointments', '2023-06-07 12:10:41', '2023-06-07 12:10:41'),
+	(7, 'Manage users', 'manage-users', '2023-06-07 12:10:59', '2023-06-07 12:10:59'),
+	(8, 'Create Appointments', 'create-appointments', '2023-06-07 12:10:59', '2023-06-07 12:10:59'),
+	(9, 'Manage Appointments', 'manage-appointments', '2023-06-07 12:10:59', '2023-06-07 12:10:59');
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.personal_access_tokens
+CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint unsigned NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.personal_access_tokens: ~0 rows (приблизно)
+DELETE FROM `personal_access_tokens`;
+/*!40000 ALTER TABLE `personal_access_tokens` DISABLE KEYS */;
+/*!40000 ALTER TABLE `personal_access_tokens` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.roles
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.roles: ~9 rows (приблизно)
+DELETE FROM `roles`;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
+	(1, 'Administrator', 'administrator', '2023-06-07 12:10:00', '2023-06-07 12:10:00'),
+	(2, 'Medical actor', 'medical-actor', '2023-06-07 12:10:00', '2023-06-07 12:10:00'),
+	(3, 'Patient', 'patient', '2023-06-07 12:10:00', '2023-06-07 12:10:00');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.roles_permissions
+CREATE TABLE IF NOT EXISTS `roles_permissions` (
+  `role_id` bigint unsigned NOT NULL,
+  `permission_id` bigint unsigned NOT NULL,
+  PRIMARY KEY (`role_id`,`permission_id`),
+  KEY `roles_permissions_permission_id_foreign` (`permission_id`),
+  CONSTRAINT `roles_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `roles_permissions_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.roles_permissions: ~0 rows (приблизно)
+DELETE FROM `roles_permissions`;
+/*!40000 ALTER TABLE `roles_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles_permissions` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.users: ~3 rows (приблизно)
+DELETE FROM `users`;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+	(1, 'Clint Eastwood', 'clint.eastwood@mail.com', NULL, '$2y$10$Q7Wbg.U4vivgCH.RDYSpAer3HkrwTyiY3aPADH0NWrf.FIrrdwOvC', NULL, '2023-06-07 12:10:59', '2023-06-07 12:10:59'),
+	(2, 'Lee Van Cliff', 'lee.van.cliff@mail.com', NULL, '$2y$10$AokPCcXvOe1tANjwMtnIbeVsAY/uo1MA0lnHxzcX4ySLU4BjRULie', NULL, '2023-06-07 12:10:59', '2023-06-07 12:10:59'),
+	(3, 'Elly Wallah', 'elly.wallah@mail.com', NULL, '$2y$10$n3nld989sr9KQS7yWF81/efzHsJU0brZSgi0N9fgw7Qc3L7tq5F9S', NULL, '2023-06-07 12:10:59', '2023-06-07 12:10:59');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.users_permissions
+CREATE TABLE IF NOT EXISTS `users_permissions` (
+  `user_id` bigint unsigned NOT NULL,
+  `permission_id` bigint unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`permission_id`),
+  KEY `users_permissions_permission_id_foreign` (`permission_id`),
+  CONSTRAINT `users_permissions_permission_id_foreign` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `users_permissions_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.users_permissions: ~4 rows (приблизно)
+DELETE FROM `users_permissions`;
+/*!40000 ALTER TABLE `users_permissions` DISABLE KEYS */;
+INSERT INTO `users_permissions` (`user_id`, `permission_id`) VALUES
+	(1, 1),
+	(2, 2),
+	(3, 2),
+	(2, 3);
+/*!40000 ALTER TABLE `users_permissions` ENABLE KEYS */;
+
+-- Dumping structure for таблиця doctor-appointment-system.users_roles
+CREATE TABLE IF NOT EXISTS `users_roles` (
+  `user_id` bigint unsigned NOT NULL,
+  `role_id` bigint unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `users_roles_role_id_foreign` (`role_id`),
+  CONSTRAINT `users_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `users_roles_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Dumping data for table doctor-appointment-system.users_roles: ~3 rows (приблизно)
+DELETE FROM `users_roles`;
+/*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
+INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3);
+/*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
