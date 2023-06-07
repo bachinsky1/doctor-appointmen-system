d271b6a40f4f2fe4c91c6a7dcb33be2da144f6c6
Vladimir Bachinsky <v.bachinsky@kralys.ch>
2023-06-07 13:52:22 +0300

    Added docker cpmpose

---
+++ docker/database/doctor-appointment-system.sql
@@ -0,0 +1,3 @@
+-- Dumping database structure for doctor-appointment-system
+CREATE DATABASE IF NOT EXISTS `doctor-appointment-system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- USE `doctor-appointment-system`;
