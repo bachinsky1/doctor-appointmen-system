06db03ba2f6c9fcd907863c0d9a2e91f8344635d
V. Bachinsky <bachinsky1@gmail.com>
2023-06-09 11:45:40 +0300

    Appointments prototype

---
+++ app/Http/Controllers/AppointmentTypesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AppointmentTypes;
+use Illuminate\Http\Request;
+
+class AppointmentTypesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\AppointmentTypes  $appointmentTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AppointmentTypes $appointmentTypes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\AppointmentTypes  $appointmentTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AppointmentTypes $appointmentTypes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\AppointmentTypes  $appointmentTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AppointmentTypes $appointmentTypes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\AppointmentTypes  $appointmentTypes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AppointmentTypes $appointmentTypes)
+    {
+        //
+    }
+}
