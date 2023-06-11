a59a54f71ca94c7a1f553e83bad0d55ab4104079
V. Bachinsky <bachinsky1@gmail.com>
2023-06-11 20:37:28 +0300

    Fixes, improvements

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,7 @@ import router from './router'
 const pinia = createPinia()
 
 // import Agenda from './components/Agenda.vue' 
+import Symbols from './components/Symbols.vue' 
 import NextAppointments from './components/dashboard/NextAppointments.vue'
 import Activities from './components/dashboard/Activities.vue'
 import DocumentsToClassify from './components/dashboard/DocumentsToClassify.vue'
@@ -23,6 +24,7 @@ app.component('next-appointments-component', NextAppointments)
 app.component('activities-component', Activities)
 app.component('documents-to-classify-component', DocumentsToClassify)
 app.component('tasks-component', Tasks)
+app.component('symbols-component', Symbols)
 // app.component('tasks-component', Agenda);
 
 app.mount("#app")
