0e3bac260b9eb8f90b2456b3428691e8d1fdd21f
V. Bachinsky <bachinsky1@gmail.com>
2023-06-20 21:55:17 +0300

    migration changed

--- database/migrations/2023_06_09_084521_create_appointments_table.php
+++ database/migrations/2023_06_09_084521_create_appointments_table.php
@@ -21,9 +21,13 @@ return new class extends Migration
             $table->datetime('start');
             $table->datetime('end');
             $table->boolean('allDay')->default(false);
+            
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('patient_id')->nullable();
             $table->unsignedBigInteger('type_id');
+            
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('patient_id')->references('id')->on('users');
             $table->foreign('type_id')->references('id')->on('appointment_types');
 
             $table->timestamps();
