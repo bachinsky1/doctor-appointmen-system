450af17a2aa8b420a5aa9714033f9148b6033bc6
V. Bachinsky <bachinsky1@gmail.com>
2023-06-22 16:10:49 +0300

    Search by doctor's name included to searchpanel

--- app/Http/Controllers/SymptomController.php
+++ app/Http/Controllers/SymptomController.php
@@ -71,18 +71,24 @@ class SymptomController extends Controller
     {
         $symptom = $request->input('search');
         // $symptoms = DB::table('symptoms')->whereFullText('name', $query)->take(10)->get();
-        
-        $doctors = User::select('users.*', 'medicalestablishments.name as establishment_name')
+
+        $doctors = User::select('users.*', 'medicalestablishments.name as establishment_name', 'positions.name as position_name')
             ->join('user_medicalestablishments', 'users.id', '=', 'user_medicalestablishments.user_id')
             ->join('medicalestablishments', 'user_medicalestablishments.medicalestablishment_id', '=', 'medicalestablishments.id')
             ->join('positions', 'user_medicalestablishments.position_id', '=', 'positions.id')
             ->join('user_specialities', 'users.id', '=', 'user_specialities.user_id')
             ->join('symptom_specialities', 'user_specialities.speciality_id', '=', 'symptom_specialities.speciality_id')
             ->join('symptoms', 'symptom_specialities.symptom_id', '=', 'symptoms.id')
-            ->whereFullText('symptoms.name', $symptom) 
-            ->groupBy('users.id', 'medicalestablishments.id')
+            ->where(function ($query) use ($symptom) {
+                $query->whereFullText('symptoms.name', $symptom)
+                    ->orWhere('users.first_name', 'LIKE', '%' . $symptom . '%')
+                    ->orWhere('users.last_name', 'LIKE', '%' . $symptom . '%');
+            })
+            ->groupBy('users.id', 'medicalestablishments.id', 'positions.id')
             ->take(10)
             ->get();
+
+
 
         return response()->json($doctors); 
     }
--- resources/js/views/components/Search.vue
+++ resources/js/views/components/Search.vue
@@ -2,7 +2,15 @@
     <div class="relative">
         <input v-model="searchTerm" placeholder="Search..." @input="handleInput" type="search" class="w-full placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:outline-none focus:ring-theme-500 focus:border-theme-500 text-sm" id="exampleSearch" />
         <ul ref="list" v-show="!hideList && filteredItems.length > 0" class="mt-1 absolute top-full left-0 w-full bg-white border border-gray-300 border-theme-500 rounded-b-md focus:outline-none focus:ring-theme-500 focus:border-theme-500 shadow-lg">
-            <li v-for="item in filteredItems" :key="item.id" class="py-3 px-3 mr-6 cursor-pointer hover:bg-gray-100" @click="handleItemClick(item); hideList = true">{{ item.full_name }} - {{ item.establishment_name }}</li>
+            <li v-for="item in filteredItems" :key="item.id" class="py-3 px-3 mr-6 cursor-pointer hover:bg-gray-100" @click="handleItemClick(item); hideList = true">
+                {{ item.full_name }}
+                <Badge theme="success" class="inline">
+                    {{ item.position_name }}
+                </Badge>
+                <Badge theme="info" class="inline">
+                    {{ item.establishment_name }}
+                </Badge>
+            </li>
         </ul>
     </div>
 </template>
@@ -13,10 +21,12 @@ import { trans } from "@/helpers/i18n"
 import { useAuthStore } from "@/stores/auth"
 import SearchService from '@/services/SearchService'
 import SearchInput from 'vue-search-input'
+import Badge from '@/views/components/Badge'
 
 export default defineComponent({
     components: {
-        SearchInput
+        SearchInput,
+        Badge,
     },
 
     data() {
