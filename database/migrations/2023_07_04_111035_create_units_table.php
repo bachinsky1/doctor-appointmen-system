18f6f15e59f7ddf6cd832b9e80c2d96a052448cd
Vladimir Bachinsky <v.bachinsky@kralys.ch>
2023-07-04 14:25:57 +0300

    Migration fix

--- database/migrations/2023_07_04_111035_create_units_table.php
+++ database/migrations/2023_07_04_111035_create_units_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('unit');
-            $table->string('icon');
+            $table->string('icon')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
