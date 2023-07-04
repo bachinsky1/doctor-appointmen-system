46a3226122750abe4b7b9791002ac45d599e10c2
Vladimir Bachinsky <v.bachinsky@kralys.ch>
2023-07-04 15:12:58 +0300

    Fix

--- database/seeders/UnitsSeeder.php
+++ database/seeders/UnitsSeeder.php
@@ -20,7 +20,7 @@ class UnitsSeeder extends Seeder
             ['name' => 'Pressure', 'unit' => 'mmHg', 'icon' => 'fas fa-tachometer-alt', 'is_vitalsign' => true],
             ['name' => 'Breathing rate', 'unit' => 'breaths/min', 'icon' => 'fas fa-lungs', 'is_vitalsign' => true],
             ['name' => 'Peak flow', 'unit' => 'L/min', 'icon' => 'fas fa-wind', 'is_vitalsign' => true],
-            ['name' => 'Saturation', 'unit' => '%', 'icon' => 'fas fa-lungs-oxygen', 'is_vitalsign' => true],
+            ['name' => 'Saturation', 'unit' => '%', 'icon' => 'fas fa-lungs', 'is_vitalsign' => true],
             ['name' => 'PT', 'unit' => 'sec', 'icon' => 'fas fa-stopwatch', 'is_vitalsign' => true],
             ['name' => 'Glucose', 'unit' => 'mg/dL', 'icon' => 'fas fa-flask', 'is_vitalsign' => true],
             ['name' => 'HbA1c', 'unit' => '%', 'icon' => 'fas fa-notes-medical', 'is_vitalsign' => true],
